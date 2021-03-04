b2a61a5e21ec7e0a0acd77881a946b334e2f8325
jarom-plewe <jarom.plewe@jobnimbus.com>
2021-03-04 10:31:07 -0700

    fix printemployees()

--- Employee.cs
+++ Employee.cs
@@ -2,10 +2,10 @@ namespace CatWorx.BadgeMaker
 {
     class Employee
     {
-        public string FirstName;
-        public string LastName;
-        public int Id;
-        public string PhotoUrl;
+        private string FirstName;
+        private string LastName;
+        private int Id;
+        private string PhotoUrl;
 
         public Employee (string firstName, string lastName, int id, string photoUrl)
         {
@@ -14,10 +14,17 @@ namespace CatWorx.BadgeMaker
             Id = id;
             PhotoUrl = photoUrl;
         }
-
+        public int GetId()
+        {
+            return Id;
+        }
         public string GetName()
         {
-            return FirstName + " " + LastName + ", " + Id + ", " + PhotoUrl;
+            return FirstName + " " + LastName;
+        }
+        public string GetPhotoUrl()
+        {
+            return PhotoUrl;
         }
     }
 }
